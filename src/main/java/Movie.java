af8122fa35387d03947c049b82202e74838fd6dc
Dary <Dafna_pelmeshka@mail.ru>
2023-05-21 03:03:36 +0300

    Initial commit

---
+++ src/main/java/Movie.java
@@ -0,0 +1,11 @@
+public class Movie {
+    private int movieId; // идентиф.номер фильма
+    private String movieName; //название фильма
+    private String movieType; //жанр фильма
+
+    public Movie(int movieId, String movieName, String movieType) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.movieType = movieType;
+    }
+}
